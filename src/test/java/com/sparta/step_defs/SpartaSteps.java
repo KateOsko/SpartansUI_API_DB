18d5bac9dedc4314adaa5cf019e4004c5a4d705f
ekaterinaostapenko <ekaterina.m.ostapenko@gmail.com>
2022-08-20 10:43:15 -0400

    commit from Ekaterina

--- src/test/java/com/sparta/step_defs/SpartaSteps.java
+++ src/test/java/com/sparta/step_defs/SpartaSteps.java
@@ -1,7 +1,9 @@
 package com.sparta.step_defs;
 
 import com.sparta.pojo.Spartan;
+import com.sparta.utilities.BrowserUtil;
 import com.sparta.utilities.ConfigurationReader;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,6 +13,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,12 +21,11 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class SpartaSteps {
+public class SpartaSteps extends BrowserUtil  {
 
 
-    int spartanId;
     @Given("user hits and POST a spartan on api “endpoint”")
-    public void user_hits_and_post_a_spartan_on_api_endpoint( Map<String, Object> jasonBody) {
+    public void user_hits_and_post_a_spartan_on_api_endpoint(Map<String, Object> jasonBody) {
 
 
         RestAssured.baseURI = ConfigurationReader.getProperty("spartan.api.url");
@@ -34,48 +36,49 @@ public class SpartaSteps {
                 .post("/spartans");
 
         response.prettyPrint();
-        assertEquals(HttpStatus.SC_CREATED,response.statusCode());
+        assertEquals(HttpStatus.SC_CREATED, response.statusCode());
 
         JsonPath jsonPath = response.jsonPath();
 
         assertEquals(jsonPath.getString("data.name"), jasonBody.get("name"));
         assertEquals(jsonPath.getString("data.gender"), jasonBody.get("gender"));
-        assertEquals(jsonPath.getString("data.phone"), jasonBody.get("phone")  + "" );
+        assertEquals(jsonPath.getString("data.phone"), jasonBody.get("phone") + "");
 
-        spartanId = jsonPath.getInt("data.id");
+        BrowserUtil.spartanId = jsonPath.getInt("data.id");
 
     }
+
     @When("user hit and GETs “already created” users’ info from Spartans api “endpoint”")
     public void user_hit_and_ge_ts_already_created_users_info_from_spartans_api_endpoint() {
 
         RestAssured.baseURI = ConfigurationReader.getProperty("spartan.api.url");
 
         Response response = given().accept(ContentType.JSON)
-                .pathParam("id",spartanId)
+                .pathParam("id", spartanId)
                 .get("/spartans/{id}");
 
         response.prettyPrint();
 
-        assertEquals(HttpStatus.SC_OK,response.statusCode());
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
 
         Spartan spartan = response.as(Spartan.class);
-        assertEquals(spartan.getId(),spartanId);
-        assertEquals(spartan.getName(),"Michael");
-        assertEquals(spartan.getGender(),"Male");
-        assertEquals(spartan.getPhone(),3124737289L);
+        assertEquals(spartan.getId(), spartanId);
+        assertEquals(spartan.getName(), "Michael");
+        assertEquals(spartan.getGender(), "Male");
+        assertEquals(spartan.getPhone(), 3124737289L);
 
     }
     //========================ANA======================
 
     @When("User connects to DB")
     public void user_connects_to_db() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtil.createConnection();
     }
-    @When("User gets created spartan’s info from DB")
-    public void user_gets_created_spartan_s_info_from_db() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @And("User gets created spartan’s info from DB")
+    public void userGetsCreatedSpartanSInfoFromDB() throws SQLException {
+        BrowserUtil.getSpartanInfo();
+
     }
 
     //==========================UI===========================
@@ -86,16 +89,19 @@ public class SpartaSteps {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @When("User clicks on Web Data button")
     public void user_clicks_on_web_data_button() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @When("User search spartan by “name”")
     public void user_search_spartan_by_name() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @When("User gets created spartan’s information from UI")
     public void user_gets_created_spartan_s_information_from_ui() {
         // Write code here that turns the phrase above into concrete actions
