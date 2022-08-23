8f591d92166cbd9950e5fa2740dffb75813f7f64
ekaterinaostapenko <ekaterina.m.ostapenko@gmail.com>
2022-08-22 22:05:10 -0400

    commit #3 pushed Unversioned filed

--- src/test/java/com/sparta/runner/CukesRunner.java
+++ src/test/java/com/sparta/runner/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         plugin = {
-                "json:target/cucumber.json",
+
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt" ,
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
